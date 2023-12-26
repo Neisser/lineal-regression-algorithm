aa76f14c66bfd4a07e19574cfd32621bf5ac34e3
Neisser <villa.neisser@gmail.com>
2023-12-25 22:43:06 -0500

    📖 docs: added comments in main funtion to explain code

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,39 @@ use libs::dataset::lineal_dataset::DataSet;
 use libs::newton::lineal_regression::{LinealRegression, LinealRegressionOptions};
 use libs::newton::model::Model;
 
-#[allow(dead_code)]
+#[allow(dead_code)] // Suppress warnings for unused code (likely for future use)
 fn main() {
-    println!("Hello, Lineal Regression!");
 
-    let path: &Path = Path::new("data.csv");
+    // Load data from CSV file
+    let path = Path::new("data.csv"); // Create a path to the CSV file
+    let data = DataSet::new(path); // Create a DataSet instance from the CSV data
+    println!("{:?}", data); // Print a debug representation of the DataSet
 
-    let data: DataSet = DataSet::new(path);
-    
-    println!("{:?}", data);
-
+    // Create a linear regression model
     let mut model = LinealRegression::new(
-        data,
+        data, // Use the loaded dataset
         LinealRegressionOptions {
-            epochs: 1000,
-            learning_rate: 0.01,
+            epochs: 1000, // Number of training iterations
+            learning_rate: 0.01, // Step size for gradient descent
         },
     );
 
-    let _ = model.fit();
+    // Train the model
+    let _ = model.fit(); // Fit the model to the data (discarding the return value)
 
-    println!("b: {}", model.b);
-    println!("w: {}", model.w);
-    println!("cost: {}", model.cost);
+    // Print model parameters and cost
+    println!("b: {}", model.b); // Bias term (intercept)
+    println!("w: {}", model.w); // Weight vector (coefficients)
+    println!("cost: {}", model.cost); // Final cost (error) after training
 
-    let test_path: &Path = Path::new("test.csv");
+    // Load test data
+    let test_path = Path::new("test.csv"); // Create a path to the test CSV file
+    let test_data = DataSet::new(test_path); // Create a DataSet instance from the test data
+    let test_inputs = test_data.input; // Extract input values for prediction
 
-    let test_data: DataSet = DataSet::new(test_path);
-
-    let test_inputs = test_data.input;
-
+    // Make predictions on test data
     for value in test_inputs {
-        println!("predict {}: {:?}", value, model.predict(value).unwrap()); 
+        println!("predict {}: {:?}", value, model.predict(value).unwrap()); // Predict for each input value
     }
 
 }
